964712a234668a0fb76a88951759932b5128a40a
Irina Kvasnikova <i.kvasnikova@bi.zone>
2022-01-18 13:29:24 +0700

    Ветка очищена от классов

--- src/main/java/ru/praktikum/Praktikum.java
+++
@@ -1,8 +0,0 @@
-package ru.praktikum;
-
-public class Praktikum {
-    public static void main(String[] args) {
-        Account account = new Account(args.toString());
-        account.checkNameToEmboss();
-    }
-}
