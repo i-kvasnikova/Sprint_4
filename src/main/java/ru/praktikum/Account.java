f85b53219c9666a3722d3ea8f0dfb3cc7bb38ca8
Irina Kvasnikova <i.kvasnikova@bi.zone>
2022-01-18 13:23:53 +0700

    Изменен метод проверки строки

--- src/main/java/ru/praktikum/Account.java
+++ src/main/java/ru/praktikum/Account.java
@@ -1,21 +1,20 @@
 package ru.praktikum;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Account {
     private final String name;
+    private static final String NAME_PATTERN = "^(?=[\\S\\s]{3,19}$)[\\S]+[\\s][\\S]+";
+    private static final Pattern pattern = Pattern.compile(NAME_PATTERN);
 
     public Account(String name) {
         this.name = name;
     }
 
     public boolean checkNameToEmboss() {
-        if(name != null &&
-           name.length() >= 3 &&
-           name.length() <= 19 &&
-           !name.startsWith(" ") &&
-           !name.endsWith(" ") &&
-           name.replaceAll("[^ ]", "").length() == 1) {
-            return true;
-        }
-        return false;
+        if (name == null) return false;
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
     }
 }
